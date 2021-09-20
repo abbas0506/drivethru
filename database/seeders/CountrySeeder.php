ec5f373cf40e9edc292687f186d5f0ccf228fe67
abbas <abbas.sscs@gmail.com>
2021-09-20 20:04:48 +0500

    200921

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -15,11 +15,10 @@ class CountrySeeder extends Seeder
     public function run()
     {
         //
-        Country::create(['name' => 'Pakistan', 'intro' => 'introduction will be added later', 'visarequired' => 0, 'visaduration' => 0]);
-        Country::create(['name' => 'USA', 'intro' => 'introduction will be added later', 'visarequired' => 1, 'visaduration' => 4, 'lifethere' => 'Suitable for higher study', 'step1' => '1']);
-        Country::create(['name' => 'UK', 'intro' => 'introduction will be added later', 'visarequired' => 1, 'visaduration' => 5, 'lifethere' => 'Outstanding environment', 'step1' => '1']);
-        Country::create(['name' => 'Britain', 'intro' => 'introduction will be added later', 'visarequired' => 1, 'visaduration' => 3, 'lifethere' => 'Suitable for higher study', 'step1' => '1']);
-        Country::create(['name' => 'Canada', 'intro' => 'introduction will be added later', 'visarequired' => 1, 'visaduration' => 4, 'lifethere' => 'Suitable for higher study', 'step1' => '1']);
-        Country::create(['name' => 'Germany', 'intro' => 'introduction will be added later', 'visarequired' => 0, 'visaduration' => 4, 'lifethere' => 'Suitable for higher study', 'step1' => '1']);
+        Country::create(['name' => 'USA', 'intro' => 'introduction will be added later', 'visarequired' => 1, 'visaduration' => 4, 'lifethere' => 'Suitable for higher study', 'jobdesc' => 'none', 'step1' => '1']);
+        Country::create(['name' => 'UK', 'intro' => 'introduction will be added later', 'visarequired' => 1, 'visaduration' => 5, 'lifethere' => 'Outstanding environment', 'jobdesc' => 'none', 'step1' => '1']);
+        Country::create(['name' => 'Britain', 'intro' => 'introduction will be added later', 'visarequired' => 1, 'visaduration' => 3, 'lifethere' => 'Suitable for higher study', 'jobdesc' => 'none', 'step1' => '1']);
+        Country::create(['name' => 'Canada', 'intro' => 'introduction will be added later', 'visarequired' => 1, 'visaduration' => 4, 'lifethere' => 'Suitable for higher study', 'jobdesc' => 'none', 'step1' => '1']);
+        Country::create(['name' => 'Germany', 'intro' => 'introduction will be added later', 'visarequired' => 1, 'visaduration' => 4, 'lifethere' => 'Suitable for higher study', 'jobdesc' => 'none', 'step1' => '1']);
     }
 }
