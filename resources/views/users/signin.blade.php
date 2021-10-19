c4497d9e73ca07addf15cfc61fa77a669919c0b0
abbas <abbas.sscs@gmail.com>
2021-10-19 21:53:11 +0500

    19102102

--- resources/views/users/signin.blade.php
+++ resources/views/users/signin.blade.php
@@ -99,7 +99,7 @@
    }
 
    body {
-      background-image: url("{{assest('images/bg/auth.jpg')}}");
+      background-image: url("{{asset('images/bg/auth.jpg')}}");
       /* background-image: url('storage/images/bg/auth.jpg'); */
       background-repeat: no-repeat;
       background-size: cover;
