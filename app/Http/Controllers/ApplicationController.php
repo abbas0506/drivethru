cf06d51278066ec0db8d5a805f82a1abd929f903
abbas <abbas.sscs@gmail.com>
2021-11-09 10:36:05 +0500

    09112107

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -88,7 +88,7 @@ class ApplicationController extends Controller
 
             DB::beginTransaction();
             try {
-                //$application = Application::create(['user_id' => $user->id, 'charges' => 1]);
+                $application = Application::create(['user_id' => $user->id, 'charges' => 1]);
                 foreach ($ids as $id) { //course ids
                     //InternationalApplication::create(['application_id' => $application->id, 'country_id' => $id, 'course_id' => $course_id]);
                     echo "country" . $id . ", course" . $course_id . "app" . $application->id . "<br>";
