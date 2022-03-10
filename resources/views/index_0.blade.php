51f269ecdd1b39c0bbf9281f691eaa7f470f4409
abbas <abbas.sscs@gmail.com>
2022-03-10 21:59:53 +0500

    100322

--- resources/views/index_0.blade.php
+++ resources/views/index_0.blade.php
@@ -148,7 +148,52 @@
             </video>
          </div>
    </section>
-   <section class="footer">
+   <section class="section footer">
+      <div class="newsletter">
+         <div>
+            <img src="/images/icon/newsletter.png" alt="" width="60">
+            <h1>Subscribe to Newsletter</h1>
+         </div>
+         <div>
+            <input type="text">
+            <button>Submit</button>
+         </div>
+      </div>
+      <div class="content">
+         <div class="col">
+            <img src="/images/logo/light.png" alt="">
+            <p>Drivethu.pk is a simple one stop shop for all your higher education(nationa and international) universities requirements from education counselling to vetted admission process</p>
+         </div>
+         <div class="col">
+            <h2>ADDRESS</h2>
+            <p><i data-feather="map-pin" class="feather-small feather-red"></i>Lahore Pakistan</p>
+            <p><i data-feather="phone-call" class="feather-small feather-red"></i>+92 354 75 15 152</p>
+            <p><i data-feather="mail" class="feather-small feather-red"></i>support@drivethru.pk</p>
+         </div>
+         <div class="col">
+            <h2>CONTACT US</h2>
+            <form action="" class="form">
+               <div class="row">
+                  <input type="text" placeholder="name">
+                  <input type="text" placeholder="email">
+               </div>
+               <div class="row">
+                  <input type="text" placeholder="phone">
+                  <input type="text" placeholder="subject">
+               </div>
+               <div class="row">
+                  <textarea name="" id="" rows="5" placeholder="message"></textarea>
+               </div>
+               <div class="row">
+                  <button>Submit Now</button>
+               </div>
+            </form>
+         </div>
+
+      </div>
+      <div class='copyright'>
+         @2022 All rights reserved. Developed by Drivethru.pk
+      </div>
 
    </section>
 </body>
