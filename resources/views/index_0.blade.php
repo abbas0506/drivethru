2dca2ff8552180c4f2d4fb9d3a846d71b3e0b8b4
abbas <abbas.sscs@gmail.com>
2022-03-14 21:58:29 +0500

    140322

--- resources/views/index_0.blade.php
+++ resources/views/index_0.blade.php
@@ -5,18 +5,18 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>DriveThru</title>
-   <link href="/css/index.css" rel="stylesheet">
-   <link href="/css/index_responsive.css" rel="stylesheet">
+   <link href="/public/css/index.css" rel="stylesheet">
+   <link href="/public/css/index_responsive.css" rel="stylesheet">
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-   <script src="/js/feather.min.js"></script>
+   <script src="/public/js/feather.min.js"></script>
 
 </head>
 
 <body>
    <section class="header">
       <nav>
-         <img src="/images/logo/dark.png" alt="" href="" class='app-header-logo'>
+         <img src="/public/images/logo/dark.png" alt="" href="" class='app-header-logo'>
          <div class="nav-links" id='nav-links'>
             <i data-feather="x" class="feather-small feather-red" onclick="hidemenu()"></i>
             <ul>
@@ -54,7 +54,7 @@
       <div class="row">
          <div class="col">
             <div class="ico">
-               <img src="/images/icon/3.png" alt="">
+               <img src="/public/images/icon/3.png" alt="">
             </div>
             <div class="content">
                <h1>Oppertunities</h1>
@@ -64,7 +64,7 @@
 
          <div class="col">
             <div class="ico">
-               <img src="/images/icon/2.png" alt="">
+               <img src="/public/images/icon/2.png" alt="">
             </div>
             <div class="content">
                <h1>Admissions</h1>
@@ -74,7 +74,7 @@
 
          <div class="col">
             <div class="ico">
-               <img src="/images/icon/1.png" alt="">
+               <img src="/public/images/icon/1.png" alt="">
             </div>
             <div class="content">
                <h1>Consultation</h1>
@@ -115,7 +115,7 @@
       <h1 class="title">Our Mentor</h1>
       <div class="row">
          <div class="col">
-            <img src="/images/mentor/mentor2-01.png" alt="" width=300>
+            <img src="/public/images/mentor/mentor2-01.png" alt="" width=300>
          </div>
          <div class="col">
             <div class="content">
@@ -128,19 +128,25 @@
    <section class="faq">
       <div class="row">
          <div class="col">
-            <h1>Frequently Asked Questions</h1>
-            <div class="question"> <i data-feather="bell" class="feather-small feather-red"></i> What service do you offer/provide? </div>
-            <div class="ans">Drivethru provides information about 175+ National and International Universities from 17+ countries, for FREE. Admission applying services, Career counselling, Visa services.</div>
+            <h2>Frequently Asked Questions</h2>
+            <div class="question show">
+               <div class="q"> <i data-feather="bell" class="feather-small"></i> What service do you offer/provide? </div>
+               <div class="ans">Drivethru provides information about 175+ National and International Universities from 17+ countries, for FREE. Admission applying services, Career counselling, Visa services.</div>
 
-            <div class="question"><i data-feather="bell-off" class="feather-small"></i> How much do you charge? </div>
-            <div class="ans">We provide complete information about National and International admissions, FREE OF COST. If you proceed for admission we will apply on your behalf in these Universities, then we charge $1/ University ($1=150 pkr).</div>
+            </div>
+            <div class="question">
+               <div class="q"><i data-feather="bell" class="feather-small"></i> How much do you charge? </div>
+               <div class="ans">We provide complete information about National and International admissions, FREE OF COST. If you proceed for admission we will apply on your behalf in these Universities, then we charge $1/ University ($1=150 pkr).</div>
+            </div>
+            <div class="question">
 
-            <div class="question"><i data-feather="bell" class="feather-small"></i> How can I book an appointment?</div>
-            <div class="ans">User can book an appointment with us through website You can reach us via WhatsApp in office timing 9:00-5:00 at following Contact Number. +92 316 4515249, +92 345 7515152.</div>
-
-            <div class="question"><i data-feather="bell" class="feather-small"></i> Where can I get this Information? </div>
-            <div class="ans">By creating a free login user can extract every information they want. User can also leave a message on social media profiles or user can contact our Student Advisor on mentioned WhatsApp in office timings to get complete Information.</div>
-
+               <div class="q"><i data-feather="bell" class="feather-small"></i> How can I book an appointment?</div>
+               <div class="ans">User can book an appointment with us through website You can reach us via WhatsApp in office timing 9:00-5:00 at following Contact Number. +92 316 4515249, +92 345 7515152.</div>
+            </div>
+            <div class="question">
+               <div class="q"><i data-feather="bell" class="feather-small"></i> Where can I get this Information? </div>
+               <div class="ans">By creating a free login user can extract every information they want. User can also leave a message on social media profiles or user can contact our Student Advisor on mentioned WhatsApp in office timings to get complete Information.</div>
+            </div>
 
          </div>
          <div class="col">
@@ -154,7 +160,7 @@
    <section class="footer">
       <div class="newsletter">
          <div class="left">
-            <img src="/images/icon/newsletter.png" alt="" width="60">
+            <img src="/public/images/icon/newsletter.png" alt="" width="60">
             <div class="title txt-light">Subscribe to Newsletter</div>
          </div>
          <div class="right">
@@ -165,7 +171,7 @@
       </div>
       <div class="content">
          <div class="col about">
-            <img src="/images/logo/light.png" alt="">
+            <img src="/public/images/logo/light.png" alt="">
             <p>Drivethu.pk is a simple one stop shop for all your higher education(nationa and international) universities requirements from education counselling to vetted admission process</p>
          </div>
          <div class="col address">
@@ -213,6 +219,21 @@ function showmenu() {
    // navlinks.style.right = "0";
    $('#nav-links').toggleClass('slide-left');
 }
+
+// function showOrhide(event) {
+//    event.target.toggleClass('show');
+//    event.target.toggleClass
+// }
+$('.question').click(function() {
+   $active = $(this);
+   $('.question').each(function() {
+      if ($(this).hasClass('show'))
+         $(this).removeClass('show');
+      $active.addClass('show')
+
+   })
+
+})
 </script>
 
 </html>
