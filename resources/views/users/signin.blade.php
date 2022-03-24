2a859d361c1e553d3f0ba6f1fe712062c6ed8129
abbas <abbas.sscs@gmail.com>
2022-03-24 19:34:33 +0500

    v

--- resources/views/users/signin.blade.php
+++ resources/views/users/signin.blade.php
@@ -5,10 +5,11 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link href="/css/app.css" rel="stylesheet">
-   <script src="/js/app.js"></script>
-   <script src="/js/autoformat.js"></script>
-   <script src="{{ asset('/js/feather.min.js') }}"></script>
+   <link href="{{asset('/css/app.css')}}" rel="stylesheet">
+   <script src="{{asset('/js/app.js')}}"></script>
+   <script src="{{asset('/js/autoformat.js')}}"></script>
+   <script src="{{ asset('/js/feather.min.js') }}">
+   </script>
 
    <title>DriveThru</title>
 
