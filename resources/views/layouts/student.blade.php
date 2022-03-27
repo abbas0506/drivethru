d69596839984ed9fc8de2da4874d8975a45ce620
abbas <abbas.sscs@gmail.com>
2022-03-27 21:30:57 +0500

    2703228

--- resources/views/layouts/student.blade.php
+++ resources/views/layouts/student.blade.php
@@ -5,13 +5,12 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <!-- <link href="/css/my.css" rel="stylesheet"> -->
-   <link href="/css/student.css" rel="stylesheet">
-   <link href="/css/student_responsive.css" rel="stylesheet">
-   <script src="/js/app.js"></script>
-   <script src="/js/autoformat.js"></script>
-   <script src="/js/feather.min.js"></script>
-   <script src="{{ asset('js/echarts.min.js')}}" type="text/javascript"></script>
+   <link href="{{asset('/css/student.css')}}" rel="stylesheet">
+   <link href="{{asset('/css/student_responsive.css')}}" rel="stylesheet">
+   <script src="{{asset('/js/app.js')}}"></script>
+   <script src="{{asset('/js/autoformat.js')}}"></script>
+   <script src="{{asset('/js/feather.min.js')}}"></script>
+
    <meta name="csrf-token" content="{{ csrf_token() }}" />
 
    <title>DriveThru</title>
