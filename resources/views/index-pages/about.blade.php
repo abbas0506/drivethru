532bddb084bc149944b1bfb5d47bd1a6d2521cf1
abbas <abbas.sscs@gmail.com>
2022-05-15 23:20:40 +0500

    05052203

--- resources/views/index-pages/about.blade.php
+++ resources/views/index-pages/about.blade.php
@@ -7,33 +7,43 @@
 @endsection
 @section('content')
 <section class="section-80 block">
-   <div class="title">We are pleased to welcome you at DriveThru</div>
+   <div class="title red-underline mt-5">About</div>
    <div class="row">
       <div class="col w-45 txt-j mt-4">
-         <p class="txt-14 txt-j mt-4">
-            We ensure you trust and recommended a pathway to a bright future ahead.
-            With utmost commitment and kaizen approach, we acknowledge our candidate's valuable preferences to fit in top-ranked universities.
-            The platform of DRIVETHRU will ensure the right candidate to the right university.
-            Our platform provides sufficient information and services that empower you to take a lead in your academic career.
+         <p class="txt-j mt-4">
+            Drivethru is providing admission services in both National and International admissions throughout the year. We are offering
+            admissions in 175+ National HEC Recognized Universities and Universities from 18+ International Countries. Ourteam is available 24/7, through all the mediums, to process all your admissions
          </p>
       </div>
       <div class="col">
-         <img src="{{asset('/images/about/1.jpg')}}" alt="" width="300" height="250">
+         <img src="{{asset('/images/about/1.png')}}" alt="">
       </div>
 
    </div>
    <!-- 2nd row -->
+   <div class="title red-underline mt-5">Mission</div>
    <div class="row">
-      <div class="col">
-         <img src="{{asset('/images/about/2.jpg')}}" alt="" width="350" height="250">
+      <div class="col txt-j mt-4">
+         <h1>FIND UNIVERSITY</h1>
+         <p class="txt-j mt-4">
+            Intermediate students face issues regarding decision making about their future scope of studies and which university should they go to continue their education. To make a
+            clear decision, the student must have insight about the potential universities that he/she is considering to choose for admission. Drivethru provides them with complete information of 175+ National Universities and 18+ International countries for their higher education.
+            Intermediate students face issues, regarding decision making about their future scope of studies and which university should they go to continue their education. To make a clear decision, the student must have insights about the potential universities that he/she is considering to choose for admission. .</p>
       </div>
-      <div class="col w-45 txt-j mt-4">
-         <h2>Vision</h2>
-         <p class="txt-14 txt-j mt-4">
-            DriveThru wants to excel is terms of its brand recognition throughout the country, the stronger the brand image, more likely are the chances of customer retentions and newly acquires. Our vision is to be the undisputed provider of Online services for our clients, excelling in quality of service, agility and responsiveness to the changing demands of the business.
-         </p>
-      </div>
+
    </div>
+   <div class="title red-underline mt-5">Vision</div>
+   <div class="row">
+      <div class="col txt-j mt-4">
+         <h1>FIND UNIVERSITY</h1>
+         <p class="txt-j mt-4">
+            Intermediate students face issues regarding decision making about their future scope of studies and which university should they go to continue their education. To make a
+            clear decision, the student must have insight about the potential universities that he/she is considering to choose for admission. Drivethru provides them with complete information of 175+ National Universities and 18+ International countries for their higher education.
+            Intermediate students face issues, regarding decision making about their future scope of studies and which university should they go to continue their education. To make a clear decision, the student must have insights about the potential universities that he/she is considering to choose for admission. .</p>
+      </div>
+
+   </div>
+
 </section>
 @endsection
 @section('footer')
