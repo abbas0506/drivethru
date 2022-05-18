f9d6f5c330db9f3f6da4391d7b8a9bef210e4aaf
abbas <abbas.sscs@gmail.com>
2022-05-19 00:13:21 +0500

    180522

--- resources/views/index-pages/about.blade.php
+++ resources/views/index-pages/about.blade.php
@@ -6,37 +6,37 @@
 </section>
 @endsection
 @section('content')
-<section class="section-80 block">
-   <div class="title red-underline mt-5">About</div>
-   <div class="row">
-      <div class="col w-45 txt-j mt-4">
-         <p class="txt-j mt-4">
-            Drivethru is providing admission services in both National and International admissions throughout the year. We are offering
-            admissions in 175+ National HEC Recognized Universities and Universities from 18+ International Countries. Ourteam is available 24/7, through all the mediums, to process all your admissions
+<section class="section-80 index-page block">
+   <div class="title red-underline my-5">About</div>
+   <div class="row my-5">
+      <div class="col half">
+         <p class="txt-l mt-4">
+            <span class="txt-red">Drivethru</span> is providing admission services in both National and International admissions throughout the year. We are offering
+            admissions in 175+ National HEC Recognized Universities and Universities from 18+ International Countries. Ourteam is available 24/7, through all the mediums, to process all your admissions.
          </p>
       </div>
-      <div class="col">
+      <div class="col half">
          <img src="{{asset('/images/about/1.png')}}" alt="">
       </div>
 
    </div>
    <!-- 2nd row -->
-   <div class="title red-underline mt-5">Mission</div>
+   <div class="title red-underline">Mission</div>
    <div class="row">
-      <div class="col txt-j mt-4">
-         <h1>FIND UNIVERSITY</h1>
+      <div class="col mission txt-j">
+         <h3>FIND UNIVERSITY</h3>
          <p class="txt-j mt-4">
             Intermediate students face issues regarding decision making about their future scope of studies and which university should they go to continue their education. To make a
-            clear decision, the student must have insight about the potential universities that he/she is considering to choose for admission. Drivethru provides them with complete information of 175+ National Universities and 18+ International countries for their higher education.
-            Intermediate students face issues, regarding decision making about their future scope of studies and which university should they go to continue their education. To make a clear decision, the student must have insights about the potential universities that he/she is considering to choose for admission. .</p>
+            clear decision, the student must have insight about the potential universities that he/she is considering to choose for admission. Drivethru provides them with complete information of 175+ National Universities and 18+ International countries for their higher education...
+         </p>
       </div>
 
    </div>
-   <div class="title red-underline mt-5">Vision</div>
+   <div class="title red-underline">Vision</div>
    <div class="row">
-      <div class="col txt-j mt-4">
-         <h1>FIND UNIVERSITY</h1>
-         <p class="txt-j mt-4">
+      <div class="col vision txt-j">
+         <h3>FIND UNIVERSITY</h3>
+         <p class="txt-j mt-3">
             Intermediate students face issues regarding decision making about their future scope of studies and which university should they go to continue their education. To make a
             clear decision, the student must have insight about the potential universities that he/she is considering to choose for admission. Drivethru provides them with complete information of 175+ National Universities and 18+ International countries for their higher education.
             Intermediate students face issues, regarding decision making about their future scope of studies and which university should they go to continue their education. To make a clear decision, the student must have insights about the potential universities that he/she is considering to choose for admission. .</p>
--- resources/views/index-pages/admission.blade.php
+++ resources/views/index-pages/admission.blade.php
@@ -1,59 +1,58 @@
 @extends('layouts.index')
 
 @section('header')
-<section class="admission mini_header">
+<section class="about mini_header">
    <x-index.header></x-index.header>
 </section>
 @endsection
 @section('content')
-<section class="section-80">
-   <div class="title">Admission</div>
-   <div class="row">
-      <div class="col w-45 txt-j">
-         Text not provided. Text not provided. Text not provided. Text not provided. Text not provided. Text not provided.
-         Text not provided. Text not provided. Text not provided. Text not provided. Text not provided. Text not provided.
-         Text not provided. Text not provided. Text not provided. Text not provided. Text not provided.
+<section class="section-80 index-page block">
+   <div class="title red-underline my-5">Admission</div>
+   <div class="row my-5">
+      <div class="col half">
+         <p class="txt-l mt-4">
+            <span class="txt-red">Drivethru</span> is providing admission services in both National and International admissions throughout the year. We are offering admissions in 175+ National H EC Recognized Universities and Universities from 18+ International Countries. Our team is available 24/7, through all the mediums, to process all your admissions.
+         </p>
       </div>
-      <div class="col">
-         <img src="{{asset('/images/admission/1.jpg')}}" alt="" width="350">
+      <div class="col half">
+         <img src="{{asset('/images/admission/1.png')}}" alt="">
       </div>
+
    </div>
-</section>
+   <!-- 2nd row -->
 
-<section class="section-80">
    <div class="row">
-      <div class="col w-45 block">
-         <div class="content">
-            <div class="txt-red">NATIONAL</div>
-            <div class="txt-blue txt-20 txt-b">ADMISSION</div>
-            <img src="{{asset('/images/admission/2.jpg')}}" alt="" width="300" hight="150">
-            <h2 class="mt-3">Prepare Yourself</h2>
-            <p class="txt-10 txt-j mt-4">Drivethru.pk provides complete resource packages for students applying in both National and International universities. Drivethru.pk is your partner, let us take care of all your admission processes Drivethru.pk provides complete resource packages for students applying in both National and International universities. Drivethru.pk is your partner, let us take care of all your admission processes Drivethru.pk provides complete resource packages for
-               students
-               applying
-               in both
-               National and International universities. Drivethru.pk is your partner, let us take care of all your admission processesDrivethru.pk provides complete resource packages for students applying in both National and International universities. Drivethru.pk is your partner, let us take care of all your admission processes Drivethru.pk provides complete resource packages for students applying in both National and International universities. Drivethru.pk is your partner, let us take
-               care of all your admission processes Drivethru.pk provides complete resource packages for students applying in both National and International universities. Drivethru.pk is your partner, let us take care of all your admission processes
-            </p>
+      <div class="col w-45">
+         <div class="title red-underline my-5">Prepare Yourself</div>
+         <div class="row my-5">
+            <div class="fancy-pallet">
+               <div class="ico">
+                  <img src="{{asset('/images/icons/person-message.png')}}" alt="">
+               </div>
+               <div class="content mt-4 txt-c">
+                  <p>Get ready to avail this golden opportunity of getting Information of all the National & International Uni-versities at a single platform. You can filter out the best suited options for you through a single click and can start your admission process through us. You just need to fill a single Unified application form and it's all done from your side! </p>
+               </div>
+            </div>
          </div>
-
       </div>
-      <div class="col w-45 block">
-         <div class="content">
-            <div class="txt-red">INTERNATIONAL</div>
-            <div class="txt-blue txt-20 txt-b">ADMISSION</div>
-            <img src="{{asset('/images/admission/3.jpg')}}" alt="" width="300" hight="150">
-            <h2 class="mt-3">Admission Formalities</h2>
-            <p class="txt-10 txt-j mt-4">Drivethru.pk provides complete resource packages for students applying in both National and International universities. Drivethru.pk is your partner, let us take care of all your admission processes Drivethru.pk provides complete resource packages for students applying in both National and International universities. Drivethru.pk is your partner, let us take care of all your admission processes Drivethru.pk provides complete resource packages for
-               students
-               applying in both
-               National and International universities. Drivethru.pk is your partner, let us take care of all your admission processesDrivethru.pk provides complete resource packages for students applying in both National and International universities. Drivethru.pk is your partner, let us take care of all your admission processes Drivethru.pk provides complete resource packages for students applying in both National and International universities. Drivethru.pk is your partner, let us take
-               care of all your admission processes Drivethru.pk provides complete resource packages for students applying in both National and International universities. Drivethru.pk is your partner, let us take care of all your admission processes
-            </p>
+      <div class="col w-45">
+         <div class="title red-underline my-5">Admission Formalities</div>
+         <div class="row my-5">
+            <div class="fancy-pallet">
+               <div class="ico">
+                  <img src="{{asset('/images/icons/refresh-circle.png')}}" alt="">
+               </div>
+               <div class="content mt-4 txt-c">
+                  <p>Drivethru is here to take care of all your admission formalities from Ap-plying to getting slips for your entry test in case of National Admissions and Application processing to Call let-ters to Visa processing to Airport re-ceiving for International Applicants. You just need not to worry at any step. Drivethru is here to cater with all the requirements on your behalf.
+                  </p>
+               </div>
+            </div>
          </div>
       </div>
    </div>
+
 </section>
+
 @endsection
 @section('footer')
 <section class="footer">
