e80826a5b539720d88690a46efa1ab36298d85ab
abbas <abbas.sscs@gmail.com>
2022-06-01 18:38:15 +0500

    010622

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.index')
 
 @section('header')
-<section>
+<section class="home">
    <x-index.header></x-index.header>
    <div id="image-animator">
       <img src="{{asset('/images/bg/full/6.jpg')}}" alt="">
